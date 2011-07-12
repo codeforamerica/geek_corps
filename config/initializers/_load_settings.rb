b0154e787bc558f4c8e85c761aa3aa6f2eaa335c
Dan Melton <melton.dan@gmail.com>
2011-07-12 10:02:41 -0700

    moved settings.yaml load to development/test only

--- config/initializers/_load_settings.rb
+++ config/initializers/_load_settings.rb
@@ -1,10 +1,3 @@
-# Load application settings from config/settings.yml
-settings_yml = HashWithIndifferentAccess.new(YAML.load_file(Rails.root.join('config', 'settings.yml')))
-
-merged_settings = settings_yml['common']
-merged_settings.deep_merge!(settings_yml[Rails.env]) if settings_yml.has_key?(Rails.env)
-
-SETTINGS = merged_settings
 if Rails.env=='production'
 S3_KEY = ENV['S3_KEY']
 S3_SECRET = ENV['S3_SECRET']
@@ -16,7 +9,26 @@ SETTINGS['github']['key']= ENV['github_key']
 SETTINGS['github']['secret']= ENV['github_secret']
 SETTINGS['facebook']['key']= ENV['facebook_key']
 SETTINGS['facebook']['secret']= ENV['facebook_secret']
+SETTINGS['common']['providers']= ['twitter','open_id','google','facebook','linked_in','github']
+SETTINGS['organization']['name']= 'Org Name'
+SETTINGS['organization']['city']= 'City'
+SETTINGS['organization']['state']= 'State'
+SETTINGS['organization']['url']= 'http://geeks.codeforamerica.org'
+SETTINGS['organization']['twitter']= 'codeforamerica'
+SETTINGS['organization']['email']= 'corps@codeforamerica.org'
+SETTINGS['organization']['host']= 'heroku'
+SETTINGS['organization']['host_url']= 'geeks@codeforamerica.org'
+SETTINGS['organization']['logotype']= 'GeekCorps'
 else
+  # Load application settings from config/settings.yml
+  settings_yml = HashWithIndifferentAccess.new(YAML.load_file(Rails.root.join('config', 'settings.yml')))
+
+  merged_settings = settings_yml['common']
+  merged_settings.deep_merge!(settings_yml[Rails.env]) if settings_yml.has_key?(Rails.env)
+
+  SETTINGS = merged_settings
+  
   S3_KEY = SETTINGS['s3']['access_key_id']
   S3_SECRET = SETTINGS['s3']['secret_access_key']
 end
+
