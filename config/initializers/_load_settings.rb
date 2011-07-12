0ef03db97969ccfb7f005aba138a70eccfb7bb0b
Dan Melton <melton.dan@gmail.com>
2011-07-12 09:51:45 -0700

    Added production settings for heroku

--- config/initializers/_load_settings.rb
+++ config/initializers/_load_settings.rb
@@ -8,6 +8,14 @@ SETTINGS = merged_settings
 if Rails.env=='production'
 S3_KEY = ENV['S3_KEY']
 S3_SECRET = ENV['S3_SECRET']
+SETTINGS['twitter']['key']= ENV['twitter_key']
+SETTINGS['twitter']['secret']= ENV['twitter_secret']
+SETTINGS['linkedin']['key']= ENV['linkedin_key']
+SETTINGS['linkedin']['secret']= ENV['linkedin_secret']
+SETTINGS['github']['key']= ENV['github_key']
+SETTINGS['github']['secret']= ENV['github_secret']
+SETTINGS['facebook']['key']= ENV['facebook_key']
+SETTINGS['facebook']['secret']= ENV['facebook_secret']
 else
   S3_KEY = SETTINGS['s3']['access_key_id']
   S3_SECRET = SETTINGS['s3']['secret_access_key']
