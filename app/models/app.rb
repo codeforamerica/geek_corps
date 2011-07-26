bba4f0307b6f8502c1380c0ade204f9d94ef6bb2
Justin Stoller <justin.stoller@gmail.com>
2011-07-26 15:29:06 -0700

    add photos to apps

--- app/models/app.rb
+++ app/models/app.rb
@@ -3,8 +3,35 @@ class App < ActiveRecord::Base
   has_many :details
   has_many :team_members
   has_many :members, :through => :team_members, :source => :user, :uniq => true
+  has_many :roles, :as => :rolable
+  has_many :skills, :as => :skillable
+  has_many :languages, :as => :polyglot
 
   validates_uniqueness_of :name, :on => :create, :message => "must be unique"
+
+  attr_accessor :photo_import_url
+
+  PHOTO_DEFAULTS = ["#{Rails.root.to_s}/public/images/default_app_1.png",
+                    "#{Rails.root.to_s}/public/images/default_app_2.png",
+                    "#{Rails.root.to_s}/public/images/default_app_3.png"]
+
+  PHOTO_SIZES = {:medium => 220, :thumb => 48 }
+
+  has_attached_file :photo,
+    :storage => :s3,
+    :bucket => 'geekcorps_' + Rails.env,
+    :path => "/:id/:filename",
+    :s3_credentials => {
+      :access_key_id => S3_KEY,
+      :secret_access_key => S3_SECRET
+    },
+    :default_url => '/images/geekcorpsavatar1.png'
+
+  before_validation :on => :create do
+    if self.photo.nil?
+      self.photo = random_default
+    end
+  end
 
   # shouldn't these be scopes?
   def core_team_members
@@ -21,6 +48,12 @@ class App < ActiveRecord::Base
       total_members << team.members
     end
     total_members.uniq!
+  end
+
+  private
+
+  def random_default
+    File.new(PHOTO_DEFAULTS.sample)
   end
 
 end
