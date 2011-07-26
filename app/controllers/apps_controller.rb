29217ab618dc98e496fbc3cb038c5c7657b035ed
Justin Stoller <justin.stoller@gmail.com>
2011-07-26 15:31:58 -0700

    flesh out search for apps and people

--- app/controllers/apps_controller.rb
+++ app/controllers/apps_controller.rb
@@ -1,6 +1,10 @@
 class AppsController < InheritedResources::Base
   before_filter :require_admin!, :except => [:show, :index]
 
+  def index
+    @search = App.search(params[:search])
+    @apps = @search.paginate
+  end
   def show
     if params[:id].to_i==0
       @app = Team.where(:name => params[:id].downcase).first.app
