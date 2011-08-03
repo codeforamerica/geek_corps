3564b2da705b2d7a4448196aebcf7a02b6ad9843
Justin Stoller <justin.stoller@gmail.com>
2011-08-03 10:54:09 -0700

    added validation to details

--- app/models/detail.rb
+++ app/models/detail.rb
@@ -3,4 +3,10 @@ class Detail < ActiveRecord::Base
   belongs_to :team
 
   validates_uniqueness_of :name, :on => :create, :scope => [:app_id, :team_id]
+  validates_presence_of :name, :setting, :app, :team
+
+  before_validation :on => :create do
+    self.app ||= self.team.app
+    self.team ||= self.app.core_team
+  end
 end
