731f7f88c887a80d1d302936dfcdf5d50c18540f
Justin Stoller <justin.stoller@gmail.com>
2011-08-03 11:00:50 -0700

    added validation for team_members

--- app/models/team_member.rb
+++ app/models/team_member.rb
@@ -2,4 +2,10 @@ class TeamMember < ActiveRecord::Base
   belongs_to :user
   belongs_to :team
   belongs_to :app
+
+  validates_presence_of :user, :team, :app
+
+  before_validation :on => :create do
+    self.app ||= self.team.app
+  end
 end
