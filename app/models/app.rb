783e35173ce21c4d7ab848f745ab4b01fb6bcf61
Justin Stoller <justin.stoller@gmail.com>
2011-08-03 10:50:31 -0700

    added/factored relations for ease of auth/editing of app info
    updated metric partial with changed relation name members => users

--- app/models/app.rb
+++ app/models/app.rb
@@ -1,23 +1,30 @@
 class App < ActiveRecord::Base
   has_many :teams
+  has_many :regional_teams, :class_name => "Team", :conditions => {:team_type => 'application'}
   has_one  :core_team, :class_name => "Team", :conditions => {:team_type => 'core'}
-  has_many :details
+  has_many :core_team_members, :through => :core_team, :source => :team_members
   has_many :team_members
-  has_many :members, :through => :team_members, :source => :user, :uniq => true
+  has_many :users, :through => :team_members, :uniq => true
+
+  has_many :details
   has_many :roles, :as => :rolable
   has_many :skills, :as => :skillable
   has_many :languages, :as => :polyglot
+
   has_many :deploy_tasks
+  has_many :goals
+  has_many :milestones
+  has_many :steps
+
+  has_many :deploy_task_resources, :through => :deploy_tasks
+
+  accepts_nested_attributes_for :details, :deploy_tasks, :goals, :milestones, :steps
 
   validates_uniqueness_of :name, :on => :create, :message => "must be unique"
-
-  attr_accessor :photo_import_url
 
   PHOTO_DEFAULTS = ["#{Rails.root.to_s}/public/images/default_app_1.png",
                     "#{Rails.root.to_s}/public/images/default_app_2.png",
                     "#{Rails.root.to_s}/public/images/default_app_3.png"]
-
-  PHOTO_SIZES = {:medium => 220, :thumb => 48 }
 
   has_attached_file :photo,
     :storage => :s3,
@@ -43,23 +50,9 @@ class App < ActiveRecord::Base
     end
   end
 
-  # shouldn't these be scopes?
-  def core_team_members
-    self.teams.where(:team_type => "core").first.team_members
+  def admin?(user)
+    core_team.admins.include? user
   end
-
-  def regional_teams
-    self.teams.where(:team_type => "application")
-  end
-
-  def all_members
-    total_members = []
-    self.teams.each do |team|
-      total_members << team.members
-    end
-    total_members.uniq!
-  end
-
   private
 
   def random_default
