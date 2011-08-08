100419cdc2439eb9d95d8563a9c365980c993864
Justin Stoller <justin.stoller@gmail.com>
2011-08-08 11:54:20 -0700

    changed to using transactions in db

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -35,7 +35,7 @@ Spork.prefork do
     # If you're not using ActiveRecord, or you'd prefer not to run each of your
     # examples within a transaction, remove the following line or assign false
     # instead of true.
-    config.use_transactional_fixtures = false
+    config.use_transactional_fixtures = true
   end
 
 end
