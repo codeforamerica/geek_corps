e49de9529f8c16bac314d292ec94715ac9a94e9f
Dan Melton <melton.dan@gmail.com>
2011-08-09 14:24:09 -0700

    Duohhh

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,6 +3,8 @@ require 'faker'
 require 'factory_girl_rails'
 require 'uuid'
 
+skills = ['ruby', 'python', 'javascript', 'celery', 'juggling', 'node.js', 'design', 'art', 'user experience', 'fundraising', 'marketing', 'php', 'wordpress', 'drupal','couchdb', '.net', 'mongodb', 'mysql'] 
+
 #Add Regions
 puts "Adding Regions"
 [["San Francisco", "CA", "SF"], ["Seattle", "WA", "SEA"], ["Boston", "MA", "BOS"], ["Philadelphia", "PA", "PHL"]]. each do |city|
@@ -15,8 +17,11 @@ Region.all.each do |region|
   puts "Creating 20 users for #{region.city}, #{region.state}"
   20.times do |i|
     user = Factory(:user, :region_id => region.id)
-    Factory(:person, :user => user)
+    Factory(:person, :user => user, :location => region.id)
     Factory(:authentication, :user => user, :provider => 'facebook')
+    person = user.reload.person
+    (rand(6)+1).times { |x| person.skill_list << skills.shuffle.last }
+    person.save
   end
 end
 
