81c7bf85405afe2a106f3752f9c602542ea5fc2e
John Mertens <john.mertens@gmail.com>
2011-08-10 20:04:33 -0700

    Updated hoverexpand version

--- public/javascripts/lib/jquery.hoverexpand.js
+++ public/javascripts/lib/jquery.hoverexpand.js
@@ -1,26 +1,39 @@
 (function($) {
   $.fn.hoverexpand = function(options) {
     var defaults = {
-      minHeight: '100px'
+      minHeight: '100px',           // If an element has a height greater than minHeight, it will become 'expandable'
+      collapsedClass: 'expand-me',  // Class to be added to 'expandable' elements. The class is removed at the end of the expand event
+      hoverTime: 500                // Time (in ms) that a user must hover over an element before it expands
     };
     var options = $.extend(defaults, options);
 
     return this.each(function() {
       var $obj = $(this);
       var origHeight = $obj.css('height');
+      var timer = null;
       
       if( parseInt(origHeight) > parseInt(options.minHeight)) {
         $obj.css({
           height: options.minHeight,
           overflow: 'hidden'
-        });
-      
+        }).addClass(options.collapsedClass);
+        
         $obj.hover(
-          function() {
-            $obj.animate({height: origHeight }, 500);       //expand
+          function() {  // mouseover
+            if(!timer) {
+              timer = window.setTimeout(function() { 
+                $obj.animate({height: origHeight }, 500).removeClass(options.collapsedClass);       //expand
+                timer = null;
+              }, options.hoverTime);
+            }
           },
-          function() {
-            $obj.animate({height: options.minHeight}, 500); //contract
+          function() {  // mouseout
+            if(timer) {
+              window.clearTimeout(timer);
+              timer = null;
+            } else {
+              $obj.animate({height: options.minHeight}, 500).addClass(options.collapsedClass);    //contract
+            }
           }
         );
       }
