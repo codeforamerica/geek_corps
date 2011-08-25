52ecdb4b9cf6e6ba9285faa919e021a1321108c0
Dan Melton <melton.dan@gmail.com>
2011-08-25 09:15:59 -0700

    adding order change for john

--- app/models/milestone.rb
+++ app/models/milestone.rb
@@ -1,6 +1,7 @@
 class Milestone < DeployTask
   has_many :steps, :foreign_key => :parent_id, :inverse_of => :milestone
   belongs_to :app
+  scope :updated_at
 
   def skill_list
     skills = steps.inject([]) do |a,b|
