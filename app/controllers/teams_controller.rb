6d4471146d180655a37cbafd8a562c62a382f920
Ryan Resella <ryan@codeforamerica.org>
2011-11-07 12:01:25 -0800

    Remove teams app id and region from teams controller

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -2,9 +2,8 @@ class TeamsController < InheritedResources::Base
   before_filter :authenticate_user!, :except => [:show, :index, :people]
 
   def create
-    app = App.where(:id => params[:app_id]).first
-    @team = Team.new(:region => current_user.region, :app => app, :team_type => "application")
-    @team.team_members.build(:user => current_user, :team_role => "organizer", :admin => true, :app => app)
+    @team = Team.new
+    @team.team_members.build(:user => current_user, :team_role => "organizer", :admin => true)
     create!
   end
 
