da6d30d4753990466c8fa3fd441741c9d413220d
Dan Melton <melton.dan@gmail.com>
2011-11-07 11:41:05 -0800

    Removed milestones

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ GeekCorps::Application.routes.draw do
 
 
   resources :languages
-  resources :skills  resources :regions
+  resources :skills  
+  resources :regions
 
   match 'apps' => 'apps#index', :as => 'apps'
 
@@ -46,28 +47,12 @@ GeekCorps::Application.routes.draw do
 
   put 'team_members/:id' => 'team_members#update', :as => 'team_member'
 
-  resources :milestones
-  resources :steps
-  resources :deploy_task_resources
-
   post "comments/create"
 
   controller :teams do
     get '/:team_name' => :show
     get '/:team_name/people' => 'teams#people', :as => 'team_people'
     get '/:team_name/guide/' => 'milestones#index', :as => 'team_guide'
-    controller :milestones do
-      get '/:team_name/guide/milestone/:id/edit' => 'milestones#edit', :as => 'team_milestone_edit'      
-      get '/:team_name/guide/milestone/new' => 'milestones#new', :as => 'team_milestone_new'      
-      get '/:team_name/guide/milestone/:id' => 'milestones#show', :as => 'team_milestone'
-      get '/:team_name/guide/milestone/:deploy_task_id/resources/new' => 'deploy_task_resources#new', :as => 'team_milestone_resource_new'      
-    end
-    controller :steps do
-      get '/:team_name/guide/step/new' => 'steps#new', :as => 'team_step_new'
-      get '/:team_name/guide/step/:id/edit' => 'steps#edit', :as => 'team_step_edit'      
-      get '/:team_name/guide/step/:id' => 'steps#show', :as => 'team_step'
-      get '/:team_name/guide/step/:deploy_task_id/resources/new' => 'deploy_task_resources#new', :as => 'team_step_resource_new'            
-    end
   end
 
   get 'privacy' => 'pages#privacy'
