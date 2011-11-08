167882b1d1c0e9441a09dbccbd184e568eaba70e
Ryan Resella <ryan@codeforamerica.org>
2011-11-08 10:49:35 -0800

    Added region to test

--- spec/controllers/teams_controller_spec.rb
+++ spec/controllers/teams_controller_spec.rb
@@ -15,6 +15,7 @@ describe TeamsController do
      end
 
      it "should create a team" do
+       @team.region.should == "(37.75459549838729, -122.60032236194616)(37.78933228160323, -122.2899585924149)(37.59590195263007, -122.2624927721024)(37.576313499982945, -122.56736337757116)"
        Team.all.size.should == 1
      end
 
