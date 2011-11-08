6b3008c96f30cd6e7e4cce7e422535f2ed97bb11
Ryan Resella <ryan@codeforamerica.org>
2011-11-07 16:51:05 -0800

    Making create teams work

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -1,12 +1,6 @@
 class TeamsController < InheritedResources::Base
   before_filter :authenticate_user!, :except => [:show, :index, :people]
 
-  def create
-    @team = Team.new
-    @team.team_members.build(:user => current_user, :team_role => "organizer")
-    create!
-  end
-
   def show
     find_team
   end
