b7f20e9d4c5d8e6d76e38a073579f6071378361d
Ryan Resella <ryan@codeforamerica.org>
2011-11-10 13:16:08 -0800

    Update application boot for bundler args

--- config/application.rb
+++ config/application.rb
@@ -1,13 +1,15 @@
 require File.expand_path('../boot', __FILE__)
 
-require 'rails/all'
+# Pick the frameworks you want:
+require "active_record/railtie"
+require "action_controller/railtie"
+require "action_mailer/railtie"
+require "active_resource/railtie"
+# require "rails/test_unit/railtie"
 
-if defined?(Bundler)
-  # If you precompile assets before deploying to production, use this line
-  Bundler.require *Rails.groups(:assets => %w(development test))
-  # If you want your assets lazily compiled in production, use this line
-  # Bundler.require(:default, :assets, Rails.env)
-end
+# If you have a Gemfile, require the gems listed there, including any gems
+# you've limited to :test, :development, or :production.
+Bundler.require *Rails.groups(:assets) if defined?(Bundler)
 
 module GeekCorps
   class Application < Rails::Application
